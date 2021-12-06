f5bb814237c81cee885ae823421882af701c3228
TBurg64 <70659543+TBurg64@users.noreply.github.com>
2021-12-06 17:29:55 -0500

    Part 2 complete for PCA

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from program1 import program1
 from program2 import program2
 from program3 import program3
 from program4 import program4
+from program5 import program5
 
 #get arguments from user input
 if sys.argv[1] == "1":
@@ -16,6 +17,9 @@ if sys.argv[1] == "3":
 
 if sys.argv[1] == "4":
     program4(sys.argv[2])
+
+if sys.argv[1] == "5":
+    program5()
 
 if len(sys.argv) < 2:
     print("ERROR: need to give an argument")
